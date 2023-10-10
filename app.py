489618c1ccd46871936a3fe285dca20a39e1402e
Jordain <jordainjoyce@hotmail.com>
2023-10-10 18:00:51 -0400

    App files

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, send_file
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def process_image():
+    if request.method == 'POST':
+        uploaded_image = request.files['file']
+        if uploaded_image:
+            try:
+                img = Image.open(uploaded_image)
+                img1 = img.resize((157, 121))
+
+                canvas_width = 1080
+                canvas_height = 720
+                canvas = Image.new('RGB', (canvas_width, canvas_height), 'white')
+                canvas.paste(img1, (716, 554))
+
+                output_buffer = io.BytesIO()
+                canvas.save(output_buffer, format='JPEG')
+                output_buffer.seek(0)
+
+                return send_file(output_buffer, as_attachment=True, download_name='output_canvas.jpg')
+            except Exception as e:
+                return "Error processing image: " + str(e)
+        else:
+            return "No file uploaded."
+
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
